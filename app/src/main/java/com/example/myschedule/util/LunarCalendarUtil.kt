6f6a313f434ae7754c67e679ca2ac610d386aa29
An-K4 <ana5k44nknd@gmail.com>
2026-06-09 16:23:29 +0700

    refactor: clean redundant codes

--- app/src/main/java/com/example/myschedule/util/LunarCalendarUtil.kt
+++ app/src/main/java/com/example/myschedule/util/LunarCalendarUtil.kt
@@ -1,6 +1,7 @@
 package com.example.myschedule.util
 
 import java.time.LocalDate
+import kotlin.math.sin
 
 /**
  * Chuyển đổi dương lịch → âm lịch Việt Nam.
@@ -80,25 +81,25 @@ object LunarCalendarUtil {
 
         var Jd1 = 2415020.75933 + 29.53058868 * k
         Jd1 += 0.0001178 * T2 - 0.000000155 * T3
-        Jd1 += 0.00033 * Math.sin((166.56 + 132.87 * T - 0.009173 * T2) * dr)
+        Jd1 += 0.00033 * sin((166.56 + 132.87 * T - 0.009173 * T2) * dr)
 
         val M = 359.2242 + 29.10535608 * k - 0.0000333 * T2 - 0.00000347 * T3
         val Mpr = 306.0253 + 385.81691806 * k + 0.0107306 * T2 + 0.00001236 * T3
         val F = 21.2964 + 390.67050646 * k - 0.0016528 * T2 - 0.00000239 * T3
 
-        var C1 = (0.1734 - 0.000393 * T) * Math.sin(M * dr)
-        C1 += 0.0021 * Math.sin(2 * dr * M)
-        C1 -= 0.4068 * Math.sin(Mpr * dr)
-        C1 += 0.0161 * Math.sin(2 * dr * Mpr)
-        C1 -= 0.0004 * Math.sin(3 * dr * Mpr)
-        C1 += 0.0104 * Math.sin(2 * dr * F)
-        C1 -= 0.0051 * Math.sin((M + Mpr) * dr)
-        C1 -= 0.0074 * Math.sin((M - Mpr) * dr)
-        C1 += 0.0004 * Math.sin((2 * F + M) * dr)
-        C1 -= 0.0004 * Math.sin((2 * F - M) * dr)
-        C1 -= 0.0006 * Math.sin((2 * F + Mpr) * dr)
-        C1 += 0.0010 * Math.sin((2 * F - Mpr) * dr)
-        C1 += 0.0005 * Math.sin((M + 2 * Mpr) * dr)
+        var C1 = (0.1734 - 0.000393 * T) * sin(M * dr)
+        C1 += 0.0021 * sin(2 * dr * M)
+        C1 -= 0.4068 * sin(Mpr * dr)
+        C1 += 0.0161 * sin(2 * dr * Mpr)
+        C1 -= 0.0004 * sin(3 * dr * Mpr)
+        C1 += 0.0104 * sin(2 * dr * F)
+        C1 -= 0.0051 * sin((M + Mpr) * dr)
+        C1 -= 0.0074 * sin((M - Mpr) * dr)
+        C1 += 0.0004 * sin((2 * F + M) * dr)
+        C1 -= 0.0004 * sin((2 * F - M) * dr)
+        C1 -= 0.0006 * sin((2 * F + Mpr) * dr)
+        C1 += 0.0010 * sin((2 * F - Mpr) * dr)
+        C1 += 0.0005 * sin((M + 2 * Mpr) * dr)
 
         val deltat = if (T < -11) {
             0.001 + 0.000839 * T + 0.0002261 * T2 - 0.00000845 * T3 - 0.000000081 * T * T3
@@ -116,9 +117,9 @@ object LunarCalendarUtil {
         val dr = Math.PI / 180
         val M = 357.52910 + 35999.05030 * T - 0.0001559 * T2 - 0.00000048 * T * T2
         val L0 = 280.46645 + 36000.76983 * T + 0.0003032 * T2
-        var DL = (1.9146 - 0.004817 * T - 0.000014 * T2) * Math.sin(dr * M)
-        DL += (0.019993 - 0.000101 * T) * Math.sin(dr * 2 * M)
-        DL += 0.00029 * Math.sin(dr * 3 * M)
+        var DL = (1.9146 - 0.004817 * T - 0.000014 * T2) * sin(dr * M)
+        DL += (0.019993 - 0.000101 * T) * sin(dr * 2 * M)
+        DL += 0.00029 * sin(dr * 3 * M)
         var L = L0 + DL
         L /= 360.0
         L -= L.toInt()
